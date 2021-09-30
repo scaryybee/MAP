5ea8e95caee21768ee655d3da2a3d7f63fd2d62f
Jaemin-MC <hino123456@outlook.kr>
2021-09-30 23:21:51 +0900

    [ + ] Fix PlayerHead Functions

--- src/main/kotlin/io/github/asr/mafp/player/PlayerHead.kt
+++ src/main/kotlin/io/github/asr/mafp/player/PlayerHead.kt
@@ -1,20 +1,27 @@
 package io.github.asr.mafp.player
 
 import org.bukkit.Material
+import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
 import org.bukkit.plugin.Plugin
 import java.util.*
 
-fun Plugin.playerHead(player: Player): ItemStack =
+fun Plugin.playerHead(name: String): ItemStack =
     ItemStack(Material.PLAYER_HEAD).apply {
         val meta = this.itemMeta as SkullMeta
-        meta.owningPlayer = server.getOfflinePlayer(player.uniqueId)
+        meta.owningPlayer = server.getPlayer(name)
     }
 
 fun Plugin.playerHead(uuid: UUID): ItemStack =
     ItemStack(Material.PLAYER_HEAD).apply {
         val meta = this.itemMeta as SkullMeta
-        meta.owningPlayer = server.getOfflinePlayer(uuid)
+        meta.owningPlayer = server.getPlayer(uuid)
+    }
+
+fun playerHead(player: Player): ItemStack =
+    ItemStack(Material.PLAYER_HEAD).apply {
+        val meta = this.itemMeta as SkullMeta
+        meta.owningPlayer = player
     }
