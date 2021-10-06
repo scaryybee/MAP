c4699700d655f1345eddfb858faff6102c897117
Jaemin-MC <hino123456@outlook.kr>
2021-10-06 13:14:18 +0900

    [ + ] Preparing jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.5.31"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    `maven-publish`
 }
 
 group = properties["group"]!!
@@ -16,15 +17,33 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
 }
 
+val shade = configurations.create("shade")
+shade.extendsFrom(configurations.implementation.get())
+
 tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+
     compileKotlin {
         kotlinOptions.jvmTarget = "16"
     }
 
-    shadowJar {
-        archiveBaseName.set(project.name)
-        archiveClassifier.set("")
-        archiveVersion.set("")
+    create<Jar>("sourceJar") {
+        archiveClassifier.set("source")
+        from(sourceSets["main"].allSource)
+    }
+
+    jar {
+        from (shade.map { if (it.isDirectory) it else zipTree(it) })
     }
 }
 
+publishing {
+    publications {
+        create<MavenPublication>(rootProject.name) {
+            from(components["java"])
+            artifact(tasks["sourceJar"])
+        }
+    }
+}
