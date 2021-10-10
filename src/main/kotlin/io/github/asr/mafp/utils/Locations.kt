503ce0e5325468684e759cf0e2e6a68665833fcd
Jaemin-MC <hino123456@outlook.kr>
2021-10-10 15:11:34 +0900

    [ + ] Not BUG But, Fix Gabs

--- src/main/kotlin/io/github/asr/mafp/utils/Locations.kt
+++ src/main/kotlin/io/github/asr/mafp/utils/Locations.kt
@@ -8,6 +8,8 @@ import org.bukkit.entity.EntityType
 import org.bukkit.event.entity.CreatureSpawnEvent
 import org.bukkit.util.Consumer
 import org.bukkit.util.Vector
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.math.sqrt
 
 fun Location.spawnEntity(type: EntityType) {
@@ -40,12 +42,36 @@ fun Location.playSound(sound: Sound, volume: Float) {
     world.playSound(this, sound, volume, pitch)
 }
 
+fun Location.NSS(radius: Double, n: Int): List<Location> {
+    val locationList = mutableListOf<Location>()
+    val angle: Double = 360.0 / n
+    for (i in 0 until n) {
+        val t = angle * i
+        val x = cos(t) * radius
+        val z = sin(t) * radius
+
+        locationList.add(clone().add(x, 0.0, z))
+    }
+
+    return locationList
+}
+
+private infix fun Int.to(to: Int) = this..to
+
 fun Location.circle(radius: Double, gab: Double): List<Location> {
     val locationList = mutableListOf<Location>()
 
-    for (i in -(radius * 100).toInt() until (radius * 100).toInt() step (gab * 100).toInt()) {
+    for (i in -(radius * 100).toInt() to (radius * 100).toInt() step (gab * 100).toInt()) {
         val xLoc = i.toDouble() / 100
         val zLoc = sqrt(radius * radius - xLoc * xLoc)
+
+        locationList.add(clone().add(xLoc, 0.0, zLoc))
+        locationList.add(clone().add(xLoc, 0.0, -zLoc))
+    }
+
+    for (i in -(radius * 100).toInt() to (radius * 100).toInt() step (gab * 100).toInt()) {
+        val zLoc = i.toDouble() / 100
+        val xLoc = sqrt(radius * radius - zLoc * zLoc)
 
         locationList.add(clone().add(xLoc, 0.0, zLoc))
         locationList.add(clone().add(xLoc, 0.0, -zLoc))
