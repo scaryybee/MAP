6869a3db1f92fe84707dfbfafc83f4ccd876c745
Jaemin-MC <hino123456@outlook.kr>
2021-10-23 08:49:42 +0900

    [ + ] Add Inventory elseEvent

--- src/main/kotlin/io/github/asr/mafp/listener/ElseEvents.kt
+++ src/main/kotlin/io/github/asr/mafp/listener/ElseEvents.kt
@@ -4,7 +4,9 @@ import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryEvent
 import org.bukkit.event.player.PlayerEvent
+import org.bukkit.inventory.Inventory
 import org.bukkit.plugin.Plugin
 
 fun Plugin.eventCall(event: Event) = server.pluginManager.callEvent(event)
@@ -23,8 +25,8 @@ fun <T : PlayerEvent> Player.elseEvent(plugin: Plugin, listener: T.() -> Unit): 
     return listenerData
 }
 
-fun <T : PlayerEvent> Plugin.elsePlayerEvent(player: Player, listener: T.() -> Unit): Listener
-    = player.elseEvent<T>(this, listener)
+fun <T : PlayerEvent> Plugin.elsePlayerEvent(player: Player, listener: T.() -> Unit): Listener =
+    player.elseEvent(this, listener)
 
 fun <T : Event> Plugin.elseEvent(listener: T.() -> Unit): Listener {
     val listenerData = object : Listener {
@@ -37,3 +39,18 @@ fun <T : Event> Plugin.elseEvent(listener: T.() -> Unit): Listener {
     server.pluginManager.registerEvents(listenerData, this)
     return listenerData
 }
+
+fun <T : InventoryEvent> Inventory.elseEvent(plugin: Plugin, listener: T.() -> Unit): Listener {
+    val listenerData = object : Listener {
+        @EventHandler
+        fun onEvent(event: T) {
+            if (event.inventory == this) listener(event)
+        }
+    }
+
+    plugin.server.pluginManager.registerEvents(listenerData, plugin)
+    return listenerData
+}
+
+fun <T : InventoryEvent> Plugin.elseInventoryEvent(inventory: Inventory, listener: T.() -> Unit) =
+    inventory.elseEvent(this, listener)
