305cb5278628282c9b99f3ce8c1139eb45de54c8
Jaemin-MC <hino123456@outlook.kr>
2021-10-24 17:09:20 +0900

    [ + ] Add Random.material, itemStack

--- src/main/kotlin/io/github/asr/mafp/random/Random.kt
+++ src/main/kotlin/io/github/asr/mafp/random/Random.kt
@@ -1,7 +1,9 @@
 package io.github.asr.mafp.random
 
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.entity.Entity
+import org.bukkit.inventory.ItemStack
 import kotlin.random.Random
 
 private fun <T> randomInArray(array: Array<T>): T = array[Random.nextInt(array.size)]
@@ -11,3 +13,20 @@ private inline fun <reified T> randomInList(list: List<T>): T = randomInArray(li
 fun randomEntity(list: List<Entity>): Entity = randomInList(list)
 
 fun Location.nearbyRandomEntity(radius: Double): Entity = randomEntity(getNearbyLivingEntities(radius).toList())
+
+private fun randomMaterial() = Material.values().random()
+
+private fun randomItemStack(): ItemStack {
+    val material = randomMaterial()
+    return ItemStack(material, Random.nextInt(material.maxStackSize) + 1)
+}
+
+class Random {
+    companion object {
+        @JvmStatic
+        fun material() = randomMaterial()
+
+        @JvmStatic
+        fun itemStack() = randomItemStack()
+    }
+}
