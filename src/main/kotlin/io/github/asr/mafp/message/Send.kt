bdec2d4245f5a62368de6b82d7392f127e833f03
PayMounth <81941006+PayMounth-Divelopur@users.noreply.github.com>
2022-01-09 22:11:16 +0900

    [ - ] Fix Bug & Delete

--- src/main/kotlin/io/github/asr/mafp/message/Send.kt
+++ src/main/kotlin/io/github/asr/mafp/message/Send.kt
@@ -1,7 +1,5 @@
 package io.github.asr.mafp.message
 
-import io.github.asr.mafp.utils.RESET
-import io.github.asr.mafp.utils.Text
 import net.kyori.adventure.text.Component
 import org.bukkit.ChatColor
 import org.bukkit.entity.Player
@@ -10,9 +8,5 @@ infix fun Component.sendTo(player: Player) = player.sendMessage(this)
 
 infix fun Component.sendTo(playerList: List<Player>) = playerList.forEach { this sendTo it }
 
-fun Component.setBaseColor(color: ChatColor): Component =
-    Component.text(color.toString() + this.insertion()!!.replace(RESET.toString(), color.toString(), true))
-
-fun Text.setBaseColor(color: ChatColor): Text =
-    this.replace(RESET.toString(), color.toString(), true).addTextFront(Text(color.toString()))
-
+infix fun Component.setBaseColor(color: ChatColor): Component =
+    Component.text("${color}${this.insertion()!!}")
