97525852dfca98345bb3ba418f7a4aef158201a1
AtSignRANK <hino123456@outlook.kr>
2022-01-10 12:01:28 +0900

    [ = ] Clear

--- src/main/kotlin/io/github/asr/mafp/message/Send.kt
+++ src/main/kotlin/io/github/asr/mafp/message/Send.kt
@@ -1,8 +1,7 @@
 package io.github.asr.mafp.message
 
-import io.github.asr.mafp.utils.RESET
-import io.github.asr.mafp.utils.Text
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextComponent
 import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 
@@ -10,9 +9,5 @@ infix fun Component.sendTo(player: Player) = player.sendMessage(this)
 
 infix fun Component.sendTo(playerList: List<Player>) = playerList.forEach { this sendTo it }
 
-fun Component.setBaseColor(color: ChatColor): Component =
-    Component.text(color.toString() + this.insertion()!!.replace(RESET.toString(), color.toString(), true))
-
-fun Text.setBaseColor(color: ChatColor): Text =
-    this.replace(RESET.toString(), color.toString(), true).addTextFront(Text(color.toString()))
-
+fun TextComponent.setBaseColor(color: ChatColor): TextComponent =
+    Component.text(this.content())
